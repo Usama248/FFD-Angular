1e807e2c672fec518cf09853f09b8ca9d77a6888
M Yasir <yasir.exalted@outlook.com>
2021-01-19 19:38:27 +0500

    routing fixes and adding links

--- src/app/pages/auth/login/basic-login/basic-login.component.ts
+++ src/app/pages/auth/login/basic-login/basic-login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-basic-login',
@@ -7,15 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicLoginComponent{
 
-constructor() {
-    console.log("basic component loaded")
-    //// redirect to home if already logged in
-    //if (this.authenticationService.currentUserValue) {
-    //  this.router.navigate(['/']);
-    //}
+  constructor(private router: Router){
   }
-
-
-
+  
+  goToPage(pageName:string){
+    this.router.navigate([`${pageName}`]);
+  }
 }
+
 
